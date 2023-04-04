238871964116ddb066ac7a8727d8597bd8796132
Muhammad Talha <talhariaz5425869@gmail.com>
2023-04-04 11:45:42 +0000

    fdgdfg

--- ssv2.js
+++ ssv2.js
@@ -1,43 +1,14 @@
 const puppeteer = require('puppeteer');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const getData = require('./downloadv2')
+
+
 let ss = require('./ss.js')
 
-function isDownloadable(url) {
-  return new Promise((resolve, reject) => {
-    const protocol = url.startsWith('https') ? https : http;
 
-    protocol
-      .request(url, { method: 'HEAD' }, (res) => {
-        const contentType = res.headers['content-type'];
-        resolve(contentType && contentType.startsWith('application'));
-      })
-      .on('error', reject)
-      .end();
-  });
-}
 
-const isDownloadLink = href => {
-  const parsed = url.parse(href);
-  const ext = path.extname(parsed.pathname);
-  return ext && (ext.toLowerCase() !== '.html' && ext.toLowerCase() !== '.htm');
-};
 module.exports= ss = async (client, id ,url) => {
 
   console.log('running ssv1')
   try {
-    await client.presenceSubscribe(id)
-
-
-		await client.sendPresenceUpdate('composing', id)
-	
-
-		await client.sendPresenceUpdate('paused', id)
-
     const browser = await puppeteer.launch({ headless: true , args: ['--no-sandbox']  });
     const page = await browser.newPage();
 
@@ -47,11 +18,7 @@ module.exports= ss = async (client, id ,url) => {
       await dialog.accept();
     });
     const links = await page.$$eval('a', links => links.map(link => ({ href: link.href || 'NULL', textContent: link.textContent || 'NULL' })));
-    const downloadLinks = links.filter(link => isDownloadLink(link.href));
-    const dLinks = []
-    downloadLinks.forEach(async(er)=>{
-        if(await isDownloadable(er)) dLinks.push(` ${er} \n\n `)
-    })
+    
     
     const data = []
     links.forEach((el)=>{
@@ -65,9 +32,7 @@ module.exports= ss = async (client, id ,url) => {
     }
     
      await client.sendMessage(id, buttonMessage)
-     if(downloadLinks.length != 0){
-    await client.sendMessage(id, {text:dLinks.join(' ')} )        
-     };
+   
   
     await browser.close();
   } catch (error) {
